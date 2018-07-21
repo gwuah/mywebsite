6053a88c9dc153f82821bd21d4e5eb1772f63f69
gwuah <griffith@dolphtech.com>
2018-07-21 23:25:17 +0000

    added homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,48 @@
 <template>
   <div class="home">
-    <nav>
-      navigation
-    </nav>
+    <navigation/>
+
     <h1 class="name">Griffith Asare Awuah</h1>
-    <p class="description">I'm a junior software engineer and fulltime javscript merchant.</p>
+    <p class="description">I'm a junior software engineer, product designer and fulltime javscript merchant.</p>
+    <p class="current-position">Currently work as a fullstack intern at <a class="aidahbot-text" href="https://www.aidahbot.com">Aidahbot Company</a></p>
+    <p class="spread-love">...</p>
+
 
     <img class="code-preview" :src="preview_img"/>
+
+    <div class="cards-container">
+
+    </div>
   </div>
 </template>
 
 <style scoped>
-  h1.name {
+  .home{
+    /* border: 1px solid green; */
+    height: 100vh;
+    box-sizing: border-box;
     font-family: 'Fira Sans Condensed', sans-serif;
-    font-size: 50px !important;
+    /* background: url(../../public/avatar.jpeg);
+    background-repeat: no-repeat;
+    background-size: cover; */
+  }
+
+  h1.name {
+    font-size: 60px !important;
   }
 
   .description {
-    position: relative;
-    top: -20px;
-    font-family: 'Fira Sans Condensed', sans-serif;
+    margin-top: -34px;
+    font-size: 20px;
+  }
+
+  .current-position, .spread-love {
+    margin-top: -10px;
+    font-size: 18px;
+  }
+
+  .current-position .aidahbot-text {
+    text-decoration: none;
   }
 
 </style>
@@ -27,10 +50,12 @@
 
 <script>
 
+import Navigation from '@/components/Navigation'
+
 export default {
   name: 'home',
   components: {
-
+    Navigation
   },
   data() {
     return {
