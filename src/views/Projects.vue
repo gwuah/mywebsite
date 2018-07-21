64ee27fb942ea70afc8c4f400adc8769c41f4aa1
gwuah <griffith@dolphtech.com>
2018-07-21 22:29:19 +0000

    created projects page

---
+++ src/views/Projects.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <navigation/>
+
+    <div class="projects">
+    This is my products page
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+
+<script>
+
+import Navigation from '@/components/Navigation'
+
+
+export default {
+  name: 'projects',
+  components: {
+    Navigation
+  },
+  data() {
+    return {
+      preview_img: "./previewg.png"
+    }
+  }
+}
+</script>
