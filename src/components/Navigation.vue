9a882c286a06bb99a3764b52c3ed0dd6b1d4e68b
gwuah <griffith@dolphtech.com>
2018-07-21 23:25:34 +0000

    add navigation component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <ul>
+      <router-link to="/">home</router-link>|
+      <router-link to="/about">about</router-link>|
+      <router-link to="/projects">projects</router-link>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+  nav {
+    margin: 70px 0;
+  }
+
+  ul a {
+    /* text-decoration:; */
+    color: black;
+    padding: 10px;
+  }
+  
+</style>
+
+<script>
+export default {
+  name: "Naviagtion"
+}
+</script>
+
+
