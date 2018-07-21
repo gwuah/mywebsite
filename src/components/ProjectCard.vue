a575e21e5150b5227712909d66c65f643ffff3c9
gwuah <griffith@dolphtech.com>
2018-07-21 22:29:42 +0000

    created custom project card

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="project-card">
+    
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+
+// props: {
+//     img: String,
+//     name: String,
+//     website: String,
+//     technologies: Array
+// }
+
+export default {
+  name: "ProjectCard",
+  props: {
+    logo: String,
+    name: String,
+    website: String,
+    technologies: Array
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
