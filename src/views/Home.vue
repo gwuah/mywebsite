64af1c1cdf337408808a49af460be4470e79a89c
gwuah <griffith@dolphtech.com>
2018-07-21 11:03:10 +0000

    main css file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav>
+      navigation
+    </nav>
+    <h1 class="name">Griffith Asare Awuah</h1>
+    <p class="description">I'm a junior software engineer and fulltime javscript merchant.</p>
+
+    <img class="code-preview" :src="preview_img"/>
   </div>
 </template>
 
+<style scoped>
+  h1.name {
+    font-family: 'Fira Sans Condensed', sans-serif;
+    font-size: 50px !important;
+  }
+
+  .description {
+    position: relative;
+    top: -20px;
+    font-family: 'Fira Sans Condensed', sans-serif;
+  }
+
+</style>
+
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+
+  },
+  data() {
+    return {
+      preview_img: "./previewg.png"
+    }
   }
 }
 </script>
