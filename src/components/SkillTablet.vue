e2a8fbd1d5c111ca66ee6bfca0c88ebaf49b4de9
gwuah <griffith@dolphtech.com>
2018-07-22 14:01:43 +0000

    redesigning

---
+++ src/components/SkillTablet.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <div :style="{backgroundColor: color || '#fc3' }">{{skill}}</div>
+  
+</template>
+
+<style scoped>
+  div {
+    /* background-color: #fc3; */
+    padding: 5px;
+    border-radius: 4px;
+  }
+</style>
+
+<script>
+export default {
+  name: "skillTablet",
+  props: {
+    skill: String,
+    color: String
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,35 @@
 <template>
   <div class="home">
-    <navigation/>
+    <navigation class="navigation"/>
 
     <h1 class="name">Griffith Asare Awuah</h1>
-    <p class="description">I'm a junior software engineer, product designer and fulltime javscript merchant.</p>
-    <p class="current-position">Currently work as a fullstack intern at <a class="aidahbot-text" href="https://www.aidahbot.com">Aidahbot Company</a></p>
+    <p class="description">I'm a junior software engineer, aspiring product designer and fulltime javscript merchant.</p>
+    <p class="current-position">Currently working as a fullstack intern at <a class="aidahbot-text" href="https://www.aidahbot.com">Aidahbot Company</a></p>
     <p class="spread-love">...</p>
 
 
-    <img class="code-preview" :src="preview_img"/>
+    <!-- <img class="code-preview" :src="preview_img"/> -->
 
-    <div class="cards-container">
-
+    <div class="skills-container">
+      <skill-tablet v-for="(skillset, key) in skills" :skill="skillset.skill" :key="key" :color="skillset.color"/>
     </div>
+
+    <div class="mini-contact">
+      Do I look like your guy? If yes, then let's talk. 
+      <a href="https://twitter.com/gwuah_"><i data-feather="twitter"></i></a>
+      <a href="https://www.linkedin.com/in/griffith-awuah-b00a62112/_"><i data-feather="linkedin"></i></a>
+      <a href="https://facebook.com/gwuah_"><i data-feather="facebook"></i></a>
+      <a href="https://github.com/gwuah_"><i data-feather="github"></i></a>
+    </div>
+
   </div>
 </template>
 
 <style scoped>
+  .navigation {
+    margin-bottom: 150px;
+  }
+
   .home{
     /* border: 1px solid green; */
     height: 100vh;
@@ -45,22 +58,67 @@
     text-decoration: none;
   }
 
+  .skills-container {
+    /* margin-top: -30px; */
+    padding: 10px;
+    width: 60%;
+    margin: 0 auto;
+    border: 1px solid #f1f1f1;
+    display: flex;
+    justify-content: space-around
+  }
+
+  .mini-contact {
+    width: 60%;
+    margin: 0 auto;
+    line-height: 80px;
+    margin-top: 25px !important;
+    border: 1px solid red;
+  }
+
+  .mini-contact svg {
+    margin: 10px;
+  }
+
 </style>
 
 
 <script>
 
-import Navigation from '@/components/Navigation'
+import Navigation from '@/components/Navigation';
+import SkillTablet from '@/components/SkillTablet';
 
 export default {
   name: 'home',
   components: {
-    Navigation
+    Navigation,
+    SkillTablet
   },
   data() {
     return {
-      preview_img: "./previewg.png"
+      preview_img: "./previewg.png",
+      skills: [
+        {skill:"reactjs", color: " #f1f1f1"},
+        {skill:"vuejs", color: null},
+        {skill:"html", color: null},
+        {skill:"css", color: null},
+        {skill:"es5/es6/es7", color: null},
+        {skill:"typescript", color: null},
+        {skill:"jquery", color: null},
+        {skill:"nodejs", color: null},
+        {skill:"express", color: null},
+        {skill:"scss", color: "#f1f1f1"},
+        {skill:"python", color: null},
+        {skill:"git", color: null},
+        {skill:"postgres", color: "#f1f1f1"},
+        {skill:"graphql", color: "#f1f1f1"}
+      ]
     }
+  },
+  mounted() {
+    // feather ? feather.replace() : alert("there's no feather")
+    // eslint-disable-next-line
+    feather.replace()
   }
 }
 </script>
